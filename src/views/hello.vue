7d2e90bceffda8473f29ed93cb78b3aef256a2a7
ZahaRehman <zaharehman788@gmail.com>
2024-10-07 18:00:04 +0500

    Reviews, Item_info and product details modalbox

--- src/views/hello.vue
+++ src/views/hello.vue
@@ -1,3 +1,20 @@
+<!-- HomeView.vue -->
 <template>
-    <h1>FoodPro</h1>
-</template>
+    <div>
+      
+      <Moreinfo />
+      <seereviews/>
+      <itemdetail/>
+    </div>
+  </template>
+  
+  <script setup>
+  import Moreinfo from '../components/Moreinfo.vue'; // Importing the modal component
+  import seereviews from '../components/SeeReviews.vue'
+  import itemdetail from '../components/itemdetail.vue'
+  </script>
+  
+  <style scoped>
+  /* Add any additional styles here for the view */
+  </style>
+  
