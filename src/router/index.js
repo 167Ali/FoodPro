7d636f2dd36a3ab6337def88b7e302639386dd29
sharifahmed07 <sharifahmed2070@gmail.com>
2024-10-07 17:52:41 +0500

    User Login/SignUp

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Hello from '../views/hello.vue'
+import Hello from '../views/Main_Landing.vue';
+import User_login from '../components/User_login.vue'; 
+import User_signup from '../components/User_signup.vue';
+
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Hello // Lazy-loaded
-      }
+  {
+    path: '/',
+    name: 'Home',
+    component: Hello 
+  },
+  {
+    path: '/User_login',
+    name: 'User_login',
+    component: User_login 
+  },
+  {
+    path: '/User_signup',
+    name: 'User_signup',
+    component: User_signup 
+  }
 ];
 
 const router = createRouter({
