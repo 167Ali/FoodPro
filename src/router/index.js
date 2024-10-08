10c754856e1a761b601ec81cddbb6b04a65add93
sharifahmed07 <sharifahmed2070@gmail.com>
2024-10-08 16:56:41 +0500

    uploading copy

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Hello from '../views/Main_Landing.vue';
 import User_login from '../components/User_login.vue'; 
 import User_signup from '../components/User_signup.vue';
-
+//import Bussiness_Landing from '../views/Bussiness_Landing.vue';
 const routes = [
   {
     path: '/',
@@ -18,7 +18,12 @@ const routes = [
     path: '/User_signup',
     name: 'User_signup',
     component: User_signup 
-  }
+  },
+  // {
+  //   path: '/Bussiness_Landing',
+  //   name: 'Bussiness_Landing',
+  //   component: Bussiness_Landing 
+  // },
 ];
 
 const router = createRouter({
